710fbd42daf8cfad5832637b471e6b19e46817b3
aboutgaurav <gaurav.kumar24@outlook.com>
2017-11-20 21:34:42 +0530

    Created basic structure.

--- app/src/main/java/com/itsgaurav/html5easy/activities/HTMLAudioActivity.java
+++ app/src/main/java/com/itsgaurav/html5easy/activities/HTMLAudioActivity.java
@@ -2,20 +2,22 @@ package com.itsgaurav.html5easy.activities;
 
 import android.os.Bundle;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.itsgaurav.html5easy.R;
+
 
 public class HTMLAudioActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-      //  setContentView(R.layout.activity_html_audio);
+        setContentView(R.layout.activity_html_audio);
 
-/*
         //For banner ads
-        AdView mAdView = (AdView) findViewById(R.id.adView_html_audio);
+        AdView mAdView = findViewById(R.id.adView_html_audio);
         AdRequest adRequest = new AdRequest.Builder().build();
         mAdView.loadAd(adRequest);
-*/
 
     }
 }
